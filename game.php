41992ade8bf851ad609da31111170a2ccf604f50
Jasper Meulendijk <jasper@webids.nl>
2017-01-30 11:00:09 +0100

    the actual game board

---
+++ game.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+		<style>
+			body {
+				background-color: blue;
+			}
+			.header {
+				color: white;
+			}
+			
+			.score {
+				float: left;
+			}
+			
+			.timer {
+				float: right;
+			}
+		</style>
+	</head>
+<body>
+	<div class="header">
+		<span class="score">Score:</span><span class="timer">Secondes:<?php $secondes ?></span>
+	</div>
+</body>
+</html>
